ba4f7f3f6ff32ad70d7986b065569a0f2d0aa8e3
dvhafly87 <dvhafly87@gmail.com>
2025-12-13 23:40:26 +0900

    암호화 페이지 30%

--- src/Accesswrapper/BoardAccessWrapper.jsx
+++ src/Accesswrapper/BoardAccessWrapper.jsx
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import API from '../config/apiConfig';
-export default function BoardPrivatePostAccess({ boardId, boardName }){
 
+
+export default function BoardPrivatePostAccess({ boardId, boardName }){
+   useEffect(() => {
+     const thisPageIsOnlyPrivateBoardPage = async () => {
+        const response = await fetch(`${API.API_BASE_URL}/board/postcalling`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                callingpostId: boardId,
+                callingpostBoardName: boardName,
+            })
+        });
+        
+     }
+     thisPageIsOnlyPrivateBoardPage();
+     }, []);
     return (
         <>
             {boardName}
