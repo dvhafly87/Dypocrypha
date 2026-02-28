ce8cb9afb5c27b38eb1323624d3e2b7f5422b82f
dvhafly87 <dvhafly87@gmail.com>
2026-02-28 23:58:59 +0900

    마이페이지 - 프로필 페이지 20%

--- src/components/MyPage/MyProfile.jsx
+++ src/components/MyPage/MyProfile.jsx
@@ -6,6 +6,34 @@ import API from '../../config/apiConfig.js';
 
 
 export default function MyProfile() {
+    const navigate = useNavigate();
+    const { addToast } = useToast();
+    const { isLogined, isLoading } = useAuth();
+
+    useEffect(() => {
+        if (isLoading) return;
+        if (!isLogined) {
+            localStorage.setItem('redirectToast', JSON.stringify({ status: 'error', message: '로그인이 필요한 서비스 입니다' }));
+            navigate('/login');
+        }
+    }, [isLogined, isLoading]);
+
+    useEffect(() => {
+        const getMemberInformation = async () => {
+            try {
+                const response = await fetch(PASSWRD_CHG_URL, {
+                    method: 'POST',
+                    credentials: 'include',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ passwordChange })
+                });
+            } catch (error) {
+
+            }
+        }
+        getMemberInformation();
+    }, []);
+
     return (
         <div className="mypage-destination">
             이 페이지는 회원 정보 페이지입니다.
